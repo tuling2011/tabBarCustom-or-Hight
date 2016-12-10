8d462ef0be8dc3987812b352dd7dde1279ee855b
tuling2011 <1575491061@qq.com>
2016-12-10 13:00:12 +0800

    Initial Commit

---
+++ dorayo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dorayo
+//
+//  Created by xuetaowang on 16/12/10.
+//  Copyright © 2016年 JinwenTechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dorayo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dorayo
+//
+//  Created by xuetaowang on 16/12/10.
+//  Copyright © 2016年 JinwenTechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
