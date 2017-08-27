410ca4cecd7f13c8cfd3e00a388eb2206c84ae4e
tuling2011 <1575491061@qq.com>
2017-08-27 12:31:05 +0800

    Revert "123"
    This reverts commit 3a1f0455ad9c38572a50677d19fb59c7d898b0d3.

--- dorayo/ViewController.m
+++ dorayo/ViewController.m
@@ -17,32 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.view.backgroundColor = [UIColor cyanColor];
-    self.title = @"123";
-    
-//使用原型图，不进行渲染
-    UIImage *selectedImage=[UIImage imageNamed: @"navigationbar_pop_highlighted"];
-    selectedImage = [selectedImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithImage:selectedImage style:UIBarButtonItemStylePlain target:self action:@selector(back)];
-//使用渲染图片
-//    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"navigationbar_pop_highlighted"] style:UIBarButtonItemStylePlain target:self action:@selector(back)];
-
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"最" style:UIBarButtonItemStylePlain target:self action:@selector(right)];
-//默认是渲染的颜色，下面改为自定义的颜色
-    self.navigationItem.rightBarButtonItem.tintColor = [UIColor yellowColor];
-    
 }
 
--(void)back{
-    
-    [self.navigationController popViewControllerAnimated:YES];
-}
--(void)right{
-    
-    [self.navigationController popViewControllerAnimated:YES];
-    
-}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
